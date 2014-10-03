6277f5b760058d990b17fcca1f9686bf26fd9649
Jakob Buis <jpabuis@gmail.com>
2014-10-03 12:37:03 +0200

    returned achmed to its roots: only pausing music, without any special, unrest-inducing features. Improved error messaging

--- application.js
+++ application.js
@@ -2,14 +2,8 @@ $(document).ready(function(){
     // Check for availability
     check_device_status();
 
-    update_status();
-
-    $('.previous', '.controls').on('click', previous);
-    $('.next', '.controls').on('click', next);
     $('.pause', '.controls').on('click', pause);
     $('.play', '.controls').on('click', play);
-
-    $('.enlightement').on('click', mandatory_enlightement);
 
     // Login is required before taking actions
     var authorization_token = getURLParameter('code');
@@ -46,11 +40,14 @@ $(document).ready(function(){
                     url: Achmed.config.oauth.endpoint + 'mp3control?access_token='+window.access_token,
                     success: function(result) {
                         $('button').removeAttr('disabled');
+                    },
+                    error: function(result) {
+                        notify('error', 'You\'re not allowed to use this');
                     }
                 });
             },
             error: function(result){
-                $('body').html(result + "Please reload.");
+                notify('error', 'Unknown fatal error occurred');
             },
         });
     }
@@ -72,153 +69,10 @@ function check_device_status()
     });
 }
 
-function mandatory_enlightement(event)
-{
-    event.preventDefault();
-
-    var jelle_wins = (Math.floor((Math.random() * 5)) == 1);
-
-    var song = '';
-    if (jelle_wins) {
-        song = "http://musicbrainz.i.bolkhuis.nl/plugin/file/files/browse/Uploads/Artists/Die Wacht am Rhein.mp3";
-    }
-    else {
-        song = "http://musicbrainz.i.bolkhuis.nl/plugin/file/files/browse/Artists/Rob de Nijs/Verzameld/0103 - Jan Klaassen de trompetter.mp3";
-    }
-    
-    // Get current song
-    $.ajax({
-        method: 'GET',
-        url: 'http://musicbrainz.i.bolkhuis.nl/player/mjs/mp3soos/current',
-        dataType: 'JSON',
-        success: function(result) {
-            // Determine if there's music playing (the playlist is not empty)
-            if (! $.isEmptyObject(result)) {
-                // Prepend song to current song
-                $.ajax({
-                    method: 'POST',
-                    url: result.url+'?access_token='+window.access_token,
-                    dataType: 'JSON',
-                    data: JSON.stringify({uri: song}),
-                    success: function() {
-                        // Press previous
-                        send_ajax('POST', '/current', {action: 'previous'});
-                    },
-                });
-            }
-            else {
-                // Append song to playlist
-                $.ajax({
-                    method: 'POST',
-                    url: 'http://musicbrainz.i.bolkhuis.nl/player/mjs/mp3soos/playlist?access_token='+window.access_token,
-                    dataType: 'JSON',
-                    data: JSON.stringify({uri: song}),
-                    success: function() {
-                        // Press play
-                        send_ajax('PUT', '/status', {status: 'playing'});
-                    },
-                });
-            }
-        },
-    });
-}
-
 function getURLParameter(name) {
     return decodeURI(
         (RegExp(name + '=' + '(.+?)(&|$)').exec(location.search)||[,null])[1]
     );
-}
-
-function update_status()
-{
-    // Get the currently playing song
-    $.ajax({
-        url: 'http://musicbrainz.i.bolkhuis.nl/player/mjs/mp3soos/current',
-        type: 'GET',
-        dataType: 'JSON',
-        error: function() {
-            notify('error', 'The music computer doesn\'t answer. It might be turned off.');
-        },
-        success: function(result){
-            if ($.isEmptyObject(result)) {
-                // No song currently playing, clear the interface
-                $('.current-song').text('No current song');
-                $('.position').val(0);
-                $('.pos_minutes, .pos_seconds, .dur_minutes, .dur_seconds').text('00');
-            }
-            else {
-                // Update position
-                update_current_song_interface(result.position, result.duration);
-
-                // Get the location of the current song
-                $.ajax({
-                    url: result.url,
-                    type: 'GET',
-                    dataType: 'JSON',
-                    success: function(result) {
-                        // Process the location of the file
-                        var file = result.location;
-                        if (file.indexOf('/pub/mp3//') === 0) {
-                            file = file.substr(10);
-                            
-                            // Get the file meta-data
-                            $.ajax({
-                                url: 'http://musicbrainz.i.bolkhuis.nl/plugin/file/files/browse/'+file,
-                                type: 'GET',
-                                dataType: 'JSON',
-                                success: function(result) {
-                                    // Update the interface
-
-                                    // Check if we have ID3-tags
-                                    if (result.artist != '' && result.title != '') {
-                                        $('.current-song').text(result.artist + ' - '+ result.title);
-                                    }
-                                    else {
-                                        // Fallback option: display filename
-                                        var pieces = file.split('/');
-                                        var name = pieces[pieces.length - 1];
-                                        if (name.length == 0) {
-                                            name = 'Unable to determine current song';
-                                        }
-                                        $('.current-song').text(name);
-                                    }
-                                },
-                            });
-                        }
-                        else {
-                            $('.current-song').text('Unable to determine current song');
-                        }
-                    },
-                });
-            }
-        },
-    });
-
-    // Do this every second
-    setTimeout(update_status, 1000);
-}
-
-function update_current_song_interface(position, duration)
-{
-    $('.position').val(position).attr('max', duration);
-    var pos_minutes = Math.floor(position / 60)
-    var pos_seconds = position % 60;
-    var dur_minutes = Math.floor(duration / 60);
-    var dur_seconds = duration % 60;
-    $('.pos_minutes').text(pos_minutes);
-    $('.pos_seconds').text(pos_seconds >= 10 ? (pos_seconds) : ('0' + pos_seconds));
-    $('.dur_minutes').text(dur_minutes);
-    $('.dur_seconds').text(dur_seconds >= 10 ? (dur_seconds) : ('0' + dur_seconds));
-}
-
-function previous()
-{
-    send_ajax('POST', '/current', {action: 'previous'});
-}
-
-function next()
-{
-    send_ajax('POST', '/current', {action: 'next'});
 }
 
 function pause()
